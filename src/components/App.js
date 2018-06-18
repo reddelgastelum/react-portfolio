b74033c9c8351329e3a81da22e9675570ba8a488
Reddel Gastelum <reddelgastelum@gmail.com>
2018-06-18 12:23:07 -0700

    init commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
-
-import '../styles/App.css';
+import HelloWorld from "./hello_world/index";
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <h1>My React App!</h1>
-            </div>
+            <HelloWorld />
         );
     }
 }
